e80fdef51e00b9a23e1ec59f8c8944ed2e2451ed
tgorsk15 <tgbusiness463@gmail.com>
2024-07-29 08:23:42 -0500

    Add "new" route and set up messages display in /index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router()
 
+const messages = [
+    {
+        text: "Howdy do?",
+        user: "Gandalf",
+        added: new Date()
+    },
+    {
+        text: "Where's Mr. Frodo?",
+        user: "Samwise",
+        added: new Date()
+    },
+    {
+        text: "Top o the mornin",
+        user: "Legolas",
+        added: new Date()
+    }
+]
+
 router.get("/", (req, res) => {
-    res.send('Wowza!!')
+    console.log(messages)
+    res.render('index', {messages: messages})
 })
 
 module.exports = router
