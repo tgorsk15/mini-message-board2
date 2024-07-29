0d6f15038f01075921943bd5692d5ecca4cb41a5
tgorsk15 <tgbusiness463@gmail.com>
2024-07-28 19:49:18 -0500

    Set up homepage router

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index.js');
 
 const app = express()
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.send('Wowza!!')
-})
+app.use("/", indexRouter)
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Howdy partner, your server is up and running'))
