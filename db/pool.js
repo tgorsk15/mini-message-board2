c3d7bda4d101dab9279910fe9882d813d08eb2b2
tgorsk15 <tgbusiness463@gmail.com>
2024-08-05 15:00:14 -0500

    Configure Pool with environment variables

--- db/pool.js
+++ db/pool.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg")
 require('dotenv').config();
 
 module.exports = new Pool({
-    host: "localhost", // or wherever the db is hosted
-    user: "tyler15",
-    database: "top_users",
-    password: "Kfkenny13!!",
-    port: 5432 // the default port
+    host: process.env.HOST, // or wherever the db is hosted
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT // the default port
 })
 
 console.log(Pool)
