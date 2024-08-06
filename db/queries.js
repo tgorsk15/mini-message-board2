0e7ffeae26b1d5c6519274db091ed3e5cc0fccd4
tgorsk15 <tgbusiness463@gmail.com>
2024-08-06 15:30:50 -0500

    Connect SQL query to add message form
    When a message is added, it is now saved internally within the database

--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,20 @@ async function getAllUsernames() {
     return rows
 }
 
+async function addMessage(text, user) {
+    await pool.query(`
+        INSERT INTO messages (text, "user", added)
+        VALUES
+            ($1, $2, CURRENT_TIMESTAMP)
+    `, [text, user])
+}
+
+async function getUserInfo(userId) {
+
+}
+
 module.exports = {
     getAllUsernames,
+    addMessage,
+    getUserInfo
 }
--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ const express = require('express');
 const router = express.Router()
 const db = require("../db/queries");
 
-const messages = [
-    {
-        text: "Howdy do?",
-        user: "Gandalf",
-        added: new Date()
-    },
-    {
-        text: "Where's Mr. Frodo?",
-        user: "Samwise",
-        added: new Date()
-    },
-    {
-        text: "Top o the mornin",
-        user: "Legolas",
-        added: new Date()
-    }
-]
+// const messages = [
+//     {
+//         text: "Howdy do?",
+//         user: "Gandalf",
+//         added: new Date()
+//     },
+//     {
+//         text: "Where's Mr. Frodo?",
+//         user: "Samwise",
+//         added: new Date()
+//     },
+//     {
+//         text: "Top o the mornin",
+//         user: "Legolas",
+//         added: new Date()
+//     }
+// ]
 
 router.get("/", async (req, res) => {
     const results = await db.getAllUsernames()
@@ -38,9 +38,10 @@ router.get('/detail/:messageId', (req, res) => {
     res.render('message', {message: message})
 })
 
-router.post('/new', (req,res) => {
-    const content = req.body
-    messages.push({text: content.message, user: content.user, added: new Date()})
+router.post('/new', async (req,res) => {
+    const content = req.body;
+    // messages.push({text: content.message, user: content.user, added: new Date()})
+    await db.addMessage(content.message, content.user)
     res.redirect("/")
 })
 
